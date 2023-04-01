7d0ada4581fda87fb7ab986d104b47882a5de74d
Kevin Griley <kevin@griley.app>
2023-04-01 09:24:44 -0700

    init

---
+++ apps/nextjs/src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import "../styles/globals.css";
+import type { AppType } from "next/app";
+
+const MyApp: AppType = ({
+  Component,
+  pageProps: { ...pageProps },
+}) => {
+  return (
+
+      <Component {...pageProps} />
+
+  );
+};
+
+export default MyApp;
---
+++ apps/nextjs/src/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>My Cartage</title>
+        <meta
+          name="description"
+          content="Advanced Airfreight TMS with Gps Tracking"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+};
+
+export default Home;
